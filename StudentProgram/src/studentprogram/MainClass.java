1f1cfb8d385e681446fe78bdee622a62c89e5b40
Sam <Sam@Sam_Computer>
2014-01-12 13:27:05 -0700

    Receive hand down code added, untested. Testing at a further date.

--- StudentProgram/src/studentprogram/MainClass.java
+++ StudentProgram/src/studentprogram/MainClass.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -40,6 +42,34 @@ public class MainClass {
         } catch (IOException ex) {
         }
 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                BufferedReader input = null;
+                try {
+                    input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                } catch (IOException ex) {
+                }
+                while (true) {
+                    try {
+                        String read = input.readLine();
+                        if (read.equals("DOWN")) {
+                            if (state == true) {
+                                try {
+                                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                                    out.println("DOWN");
+                                    button.setText("Hand is DOWN");
+                                    state = false;
+                                } catch (Exception ex) {
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         button = new JButton("Hand is DOWN");
         button.addActionListener(new ActionListener() {
             @Override
